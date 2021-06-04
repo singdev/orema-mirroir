aafcb72d8cb6589d57eb279d22d012876cf35f69
Nve Hounkponou Orphee <orpheenve@hotmail.com>
2021-06-04 09:21:17 +0200

    feat geolocalisation

--- src/app/pages/geolocalisation/geolocalisation/geolocalisation.component.ts
+++ src/app/pages/geolocalisation/geolocalisation/geolocalisation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var ol: any;
+
 @Component({
   selector: 'app-geolocalisation',
   templateUrl: './geolocalisation.component.html',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeolocalisationComponent implements OnInit {
 
+  boitierLat: number;
+  boitierLng: number;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.loadGeocalisationBoitier();
   }
 
+  loadGeocalisationBoitier(){
+    this.boitierLat = 37.41;
+    this.boitierLng = 8.82;
+    this.loadMap();
+  }
+  
+  loadMap(){
+    var map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.boitierLat, this.boitierLng]),
+        zoom: 4
+      })
+    });
+  }
+  
 }
