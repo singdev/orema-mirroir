6e57d1707ef534d1bb1186075a2b43f3f7916714
MioMia <miomia@miomia.miomia.test>
2021-08-19 13:31:55 -0700

    feat: GEO, ajouter mapbox

--- src/app/pages/geolocalisation/geolocalisation/geolocalisation.component.ts
+++ src/app/pages/geolocalisation/geolocalisation/geolocalisation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { InformationGeneralContrat } from 'src/app/model/contrat/InformationGeneraleContrat';
 import { GeolocalisationService } from 'src/app/services/geolocalisation.service';
 import { SettingService } from 'src/app/services/setting.service';
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 declare var ol: any;
 
@@ -14,8 +15,12 @@ export class GeolocalisationComponent implements OnInit {
 
   boitierLat: number;
   boitierLng: number;
-  map: any;
+
+  map: mapboxgl.Map;
+  mapStyle = "mapbox://styles/mapbox/satellite-v9";
   mapScr: string = null;
+  marker: mapboxgl.Marker;
+  mapboxAccessToken = "pk.eyJ1Ijoibmlja2V6ZWtpYXMiLCJhIjoiY2tndnFsM2hkMDJ4czM2cW16ajV3dXY4eCJ9.6yqYNQSaXhGieOf8p3_CKw";
   
   loading:boolean  = true;
 
@@ -41,7 +46,8 @@ export class GeolocalisationComponent implements OnInit {
     this.boitierLat = informationGenerales.Latitude;
     this.boitierLng = informationGenerales.Longitude;
     //this.loadMap();
-    this.setGoogleMapsView();
+    // this.setGoogleMapsView();
+    this.setMapboxView();
   }
   
   async loadGeocalisationBoitier() {
@@ -105,6 +111,22 @@ export class GeolocalisationComponent implements OnInit {
     const lat = this.boitierLng;
     const lng = this.boitierLat;
     this.mapScr = `https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15958.852102929153!2d${lat}!3d${lng}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sga!4v1629371304233!5m2!1sfr!2sga" width="600" height="450" style="border:0;"`;
+  }
+
+  setMapboxView() {
+    mapboxgl.accessToken = this.mapboxAccessToken;
+    const boitierCoordinates = [this.boitierLng, this.boitierLat]; //l'ordre lng,lat
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.mapStyle,
+      center: boitierCoordinates,
+      zoom: 15
+    });
+    // Ajouter un marqueur pour le boitier
+    const marker1 = new mapboxgl.Marker()
+    .setLngLat(boitierCoordinates)
+    .addTo(this.map);
+    this.map.addControl(new mapboxgl.NavigationControl());
   }
 
 }
